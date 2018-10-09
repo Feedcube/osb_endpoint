7f14421dba97dc20cbe2eb0e28719c743da44f32
NLindemann <niklas.lindemann@7p-group.com>
2018-10-09 08:24:26 +0200

    removed unnecessary class

--- src/main/java/de/lindemann/osb_endpoint/cache/BindingCache.java
+++
@@ -1,9 +0,0 @@
-package de.lindemann.osb_endpoint.cache;
-
-import de.lindemann.osb_endpoint.model.Binding;
-import de.lindemann.osb_endpoint.model.OsbServiceInstance;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-
